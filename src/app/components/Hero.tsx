ab4d081b1b40f015491d4306186f04d27532786b
haard18 <haardsolanki.itm@gmail.com>
2025-07-02 17:45:39 +0530

    feat: add custom font and update Hero and Navbar components to use it

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,10 +4,10 @@ import { BackgroundLines } from "./ui/background-lines";
 export function Hero() {
   return (
     <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
-    <h2 className="text-center text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight text-white drop-shadow-lg">
-      Actors Room
+    <h2 className="text-center text-2xl md:text-4xl lg:text-7xl py-2 md:py-10 relative z-20 font-bold tracking-wider" style={{ fontFamily: "THEBOLDFONT" }}>
+      The Actor&apos;s Room
     </h2>
-      <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+      <p style={{ fontFamily: "THEBOLDFONT" }} className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
         For the Actor Who Thinks, Feels, and Transforms.
       </p>
     </BackgroundLines>
--- src/app/components/ui/background-lines.tsx
+++ src/app/components/ui/background-lines.tsx
@@ -96,7 +96,7 @@ const SVG = ({
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
-      className="absolute inset-0 w-full h-full md:h-[50vh] lg:h-[75vh]"
+      className="absolute inset-0 w-full h-full "
     >
       {paths.map((path, idx) => (
         <motion.path
