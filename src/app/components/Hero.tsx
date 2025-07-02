e6a1e4fa8d37d6569d6ebb7456fb530566cfcdf0
haard18 <haardsolanki.itm@gmail.com>
2025-07-02 16:57:55 +0530

    feat: add Hero component with background lines and utility functions
    - Implemented the Hero component with a subtitle.
    - Created BackgroundLines component for animated SVG background.
    - Added utility function for class name merging.
    - Updated package.json with new dependencies: clsx, motion, and tailwind-merge.

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BackgroundLines } from "./ui/background-lines";
+
+export function Hero() {
+  return (
+    <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+    <h2 className="text-center text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight text-white drop-shadow-lg">
+      Actors Room
+    </h2>
+      <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+        For the Actor Who Thinks, Feels, and Transforms.
+      </p>
+    </BackgroundLines>
+  );
+}
